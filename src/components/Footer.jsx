e6d685ab93220237d24f2986fd8e216271ce822c
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-02-12 19:54:02 +0530

    intial changes on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import SocialIcons from "./SocialIcons";
 import GoogleMapReact from 'google-map-react';
+import { Copyright } from 'lucide-react'
 
+// Use it in your component
+ // This will make it 40x40px to match your SVG
 const SocialIcon = ({ children }) => (
   <div
     style={{
@@ -17,404 +20,81 @@ const SocialIcon = ({ children }) => (
       alignItems: "center",
     }}
   >
+
     {children}
   </div>
 );
 
+
 const Footer = () => {
   return (
-    <div style={{ width: "100%", height: "100%", position: "relative" }}>
-      <div
-        style={{
-          width: 1236,
-          height: 433,
-          left: 102,
-          top: 91,
-          position: "absolute",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 50,
-        }}
-      >
-        <div
-          style={{
-            width: 378.13,
-            height: 434,
-            position: "relative",
-            borderRadius: 20,
-            overflow: "hidden",
-            border: "2px #161616 solid",
-          }}
-        >
-          <div
-            style={{
-              width: 349,
-              left: 16.06,
-              top: 75.5,
-              position: "absolute",
-              textAlign: "center",
-              color: "#E31C25",
-              fontSize: 123,
-              fontFamily: "Vanguard",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
+    <div className="w-full h-full relative ">
+      <div className="absolute flex justify-center items-center gap-14 w-[1236px] h-[433px] left-[152px] top-[91px]">
+        <div className="relative w-[378px] h-[434px] rounded-[20px] overflow-hidden border-2 border-[#161616]">
+          <div className="absolute w-[349px] left-[16px] top-[30px] text-center text-[#E31C25] text-[123px] font-bold font-[Vanguard] ">
             CONNECT
           </div>
-          <div
-            style={{
-              left: 49.06,
-              top: 189.5,
-              position: "absolute",
-              textAlign: "center",
-              color: "#161616",
-              fontSize: 76,
-              fontFamily: "Vanguard",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
+          <div className="absolute left-[49px] top-[160px] text-center text-[#161616] text-[76px] font-bold font-[Vanguard] ">
             WITH US ON
           </div>
-          <div
-            style={{
-              padding: 8,
-              left: 23.06,
-              top: 297.5,
-              position: "absolute",
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              gap: 25,
-            }}
-          >
+          <div className="absolute flex justify-start items-center gap-6 left-[23px] top-[300px] p-2">
             <SocialIcons />
           </div>
         </div>
-        <div
-          style={{
-            width: 378.13,
-            height: 434,
-            position: "relative",
-            background: "#161616",
-            borderRadius: 20,
-            overflow: "hidden",
-            border: "2px #161616 solid",
-          }}
-        >
-          <div
-            style={{
-              width: 289,
-              left: 45.94,
-              top: 75.5,
-              position: "absolute",
-              textAlign: "center",
-              color: "#E31C25",
-              fontSize: 80,
-              fontFamily: "Vanguard",
-              fontWeight: "600",
-              wordWrap: "break-word",
-            }}
-          >
+
+        <div className="relative w-[378px] h-[434px] rounded-[20px] overflow-hidden bg-[#161616] border-2 border-[#161616]">
+          <div className="absolute w-[289px] left-[46px] top-[50px] text-center text-[#E31C25] text-[80px] font-semibold font-[Vanguard]">
             QUESTIONS?
           </div>
-          <div
-            style={{
-              left: 49.94,
-              top: 144.5,
-              position: "absolute",
-              textAlign: "center",
-            }}
-          >
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 47,
-                fontFamily: "Vanguard",
-                fontWeight: "600",
-                wordWrap: "break-word",
-              }}
-            >
-              WE
-            </span>
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 47,
-                fontFamily: "AvantGarde",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              '
-            </span>
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 47,
-                fontFamily: "Vanguard",
-                fontWeight: "600",
-                wordWrap: "break-word",
-              }}
-            >
-              RE HERE TO HELP
-            </span>
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 47,
-                fontFamily: "AvantGarde",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              !
-            </span>
+          <div className="absolute left-[50px] top-[144px] text-center text-[#FAFAFA] text-[47px] font-semibold font-vanguard">
+            WE<span className="font-bold font-avantgarde">'</span>RE HERE TO HELP<span className="font-bold font-avantgarde">!</span>
           </div>
-          <div
-            style={{
-              left: 45.94,
-              top: 229.5,
-              position: "absolute",
-              color: "#FAFAFA",
-              fontSize: 29,
-              fontFamily: "Microgramma",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            NAME
+          <div className="absolute left-[46px] top-[229px] text-[#FAFAFA] text-[29px] font-bold font-[Microgramma]">
+            AKHILJITH -
           </div>
-          <div
-            style={{
-              left: 45.94,
-              top: 309.5,
-              position: "absolute",
-              color: "#FAFAFA",
-              fontSize: 29,
-              fontFamily: "Microgramma",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            NAME
+          <div className="absolute left-[46px] top-[309px] text-[#FAFAFA] text-[29px] font-bold font-[Microgramma]">
+            EPHREM -
           </div>
-          <div
-            style={{
-              left: 86.94,
-              top: 265.5,
-              position: "absolute",
-              textAlign: "right",
-            }}
-          >
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 29,
-                fontFamily: "Microgramma",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              -
-            </span>
-            <span
-              style={{
-                color: "#E31C25",
-                fontSize: 29,
-                fontFamily: "Microgramma",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              123456789
-            </span>
+          <div className="absolute left-[87px] top-[265px] text-right text-[#FAFAFA] text-[29px] font-bold font-[Microgramma]">
+             <span className="text-[#E31C25]">7907982102</span>
           </div>
-          <div
-            style={{
-              left: 89.94,
-              top: 338.5,
-              position: "absolute",
-              textAlign: "right",
-            }}
-          >
-            <span
-              style={{
-                color: "#FAFAFA",
-                fontSize: 29,
-                fontFamily: "Microgramma",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              -
-            </span>
-            <span
-              style={{
-                color: "#E31C25",
-                fontSize: 29,
-                fontFamily: "Microgramma",
-                fontWeight: "700",
-                wordWrap: "break-word",
-              }}
-            >
-              123456789
-            </span>
+          <div className="absolute left-[90px] top-[338px] text-right text-[#FAFAFA] text-[29px] font-bold font-[Microgramma]">
+            <span className="text-[#E31C25]">9447434346</span>
           </div>
         </div>
-        <div
-          style={{
-            width: 378.13,
-            borderRadius: 20,
-            height: 434,
-            overflow: "hidden",
-            border: "2px #161616 solid",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
-          }}
-        >
-          <GoogleMapReact defaultCenter={{ lat: 8.545884830720146, lng: 76.90634321026491 }} defaultZoom={11} bootstrapURLKeys={{ key: "AIzaSyC8Nk_5APibqxRWHClK0-xBMrCxqWrGIws" }}/>
-      
 
+        <div className="relative w-[378px] h-[434px] rounded-[20px] overflow-hidden border-2 border-[#161616] flex flex-col justify-start items-center">
+          <GoogleMapReact 
+            defaultCenter={{ lat: 8.545884830720146, lng: 76.90634321026491 }} 
+            defaultZoom={11} 
+            bootstrapURLKeys={{ key: "AIzaSyC8Nk_5APibqxRWHClK0-xBMrCxqWrGIws" }}
+          />
         </div>
       </div>
-      <div style={{ left: 607, top: 615, position: "absolute" }}>
-        <div style={{ left: 45, top: 2, position: "absolute" }}>
-          <span
-            style={{
-              color: "#E31C25",
-              fontSize: 47,
-              fontFamily: "Vanguard",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            TED
-          </span>
-          <span
-            style={{
-              color: "#161616",
-              fontSize: 47,
-              fontFamily: "Vanguard",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            x CET
-          </span>
-        </div>
-        <div
-          style={{
-            width: 40,
-            height: 40,
-            left: 0,
-            top: 0,
-            position: "absolute",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-          }}
-        >
-          <div
-            style={{ width: 33.33, height: 33.33, background: "#161616" }}
-          ></div>
-        </div>
-      </div>
-      <div
-        style={{
-          width: 1600,
-          height: 0,
-          left: 0,
-          top: 597,
-          position: "absolute",
-          border: "2px #161616 solid",
-        }}
-      ></div>
-      <div
-        style={{
-          width: 1600,
-          height: 0,
-          left: 0,
-          top: 549,
-          position: "absolute",
-          border: "2px #161616 solid",
-        }}
-      ></div>
-      <div
-        style={{
-          width: 1440,
-          height: 35,
-          left: 0,
-          top: 22,
-          position: "absolute",
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            paddingLeft: 40,
-            color: "#161616",
-            fontSize: 29,
-            fontFamily: "AvantGarde",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx
-          CET - TEDx CET - TEDx CET - TEDx CET -
-        </div>
-      </div>
-      <div
-        style={{
-          width: 1600,
-          height: 0,
-          left: 0,
-          top: 66,
-          position: "absolute",
-          border: "2px #161616 solid",
-        }}
-      ></div>
-      <div
-        style={{
-          width: 1600,
-          height: 0,
-          left: 0,
-          top: 15,
-          position: "absolute",
-          border: "2px #161616 solid",
-        }}
-      ></div>
-      <div
-        style={{
-          width: 1440,
-          height: 35,
-          left: 0,
-          top: 555,
-          position: "absolute",
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            color: "#161616",
-            fontSize: 29,
-            fontFamily: "AvantGarde",
-            fontWeight: "400",
 
-          }}
-        >
-          - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx
-          CET -  TEDx CET - TEDx
-          CET -  TEDx CET -
+      <div className="relative left-[607px] top-[615px]">
+        <div className=" left-[45px] top-[2px] flex flex-row">
+           <Copyright className="w-10 h-16" />
+          <div className="text-[#E31C25] text-6xl font-bold font-[Vanguard] pl-4">TED</div>
+          <div className="text-[#161616] text-6xl font-bold font-[Vanguard]">x CET</div>
+        </div>
+       
+      </div>
+
+      <div className="absolute w-screen border-2 border-[#161616] top-[597px]"></div>
+      <div className="absolute w-screen border-2 border-[#161616] top-[549px]"></div>
+
+      <div className="absolute w-[1440px] h-[35px] left-0 top-[22px] flex justify-start items-center">
+        <div className="pl-10 text-[#161616] text-[29px] font-[AvantGarde] font-light">
+          - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET -
+        </div>
+      </div>
+      <div className="absolute w-screen border-2 border-[#161616] top-[66px]"></div>
+      <div className="absolute w-screen border-2 border-[#161616] top-[15px]"></div>
+
+      <div className="absolute w-[1440px] h-[35px] left-0 top-[555px] flex justify-end items-center">
+        <div className="text-[#161616] text-[29px] font-[AvantGarde] font-light">
+          - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET - TEDx CET -
         </div>
       </div>
     </div>
