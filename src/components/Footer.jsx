240c05d4d6296b96ef30a5c7cae1c29165725bce
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-02-18 16:42:31 +0530

    footer final change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,14 +34,14 @@ const Footer = () => {
         <ScrollingText className="w-full h-full overflow-hidden" direction="right" />
       </div>
 
-      <div className="gap-20 w-[330px] pt-20 flex flex-col space-y-4 lg:flex-row lg:justify-center lg:items-center lg:gap-20 lg:w-[1536px] lg:h-[433px] lg:left-[152px] lg:top-[91px] mx-auto ">
+      <div className="md:gap-20 gap-10 w-[330px] pt-20 flex flex-col space-y-4 lg:flex-row lg:justify-center lg:items-center lg:gap-20 lg:w-[1536px] lg:h-[433px] lg:left-[152px] lg:top-[91px] mx-auto ">
 
-        <div className=" flex flex-col justify-center items-center  h-[500px] lg:w-full lg:h-[484px] rounded-[20px] overflow-hidden border-2 border-[#161616]">
+        <div className=" flex flex-col justify-center items-center  h-[350px] lg:w-full lg:h-[484px] rounded-[20px] overflow-hidden border-2 border-[#161616]">
           <div className="lg:flex lg:flex-col flex flex-col">
-            <div className="h-[130px] pl-2 pt-3 lg:mb-2  lg:w-[400px] lg:h-[180px] text-center text-[#E31C25] lg:text-[120px] text-[90px] font-bold font-vanguard overflow-hidden ">
+            <div className="h-[110px] pl-2 pt-3 lg:mb-2  lg:w-[400px] lg:h-[180px] text-center text-[#E31C25] lg:text-[120px] text-[80px] font-bold font-vanguard overflow-hidden ">
               CONNECT
             </div>
-            <div className="pl-2 h-[120px] lg:-mt-10  text-center text text-[#161616] text-[50px] md:text-[70px] font-bold font-[Vanguard] lg:h-[160px] ">
+            <div className="pl-2 h-[120px] lg:-mt-10  text-center text text-[#161616] text-[50px] md:text-[60px] font-bold font-[Vanguard] lg:h-[160px] ">
               WITH US ON
             </div>
           </div>
@@ -50,20 +50,20 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className=" w-full h-[484px] rounded-[20px] overflow-hidden bg-[#161616] border-2 border-[#161616]">
-          <div className=" h-[120px] text-center text-[#E31C25] text-[60px] md:text-[80px] font-semibold font-[Vanguard] pt-8 justify-center items-center">
+        <div className=" w-full h-[350px] md:h-[484px] rounded-[20px] overflow-hidden bg-[#161616] border-2 border-[#161616]">
+          <div className=" h-[100px] md:h-[130px] text-center text-[#E31C25] text-[60px] md:text-[80px] font-semibold font-[Vanguard] pt-8 justify-center items-center">
             QUESTIONS?
           </div>
-          <div className=" pt-1 text-center text-[#FAFAFA] text-[30px] md:text-[40px] font-semibold font-vanguard h-[100px]">
+          <div className="h-[60px] pt-1 text-center text-[#FAFAFA] text-[30px] md:text-[40px] font-semibold font-vanguard md:h-[100px]">
             WE<span className="font-bold font-avantgarde">'</span>RE HERE TO HELP<span className="font-bold font-avantgarde">!</span>
           </div>
-          <div className=" pl-6 left-[46px] top-[229px] text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma]">
+          <div className="h-[25px] md:h-[50px] pl-16 left-[46px] top-[229px] text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma]">
             AKHILJITH -
           </div>
-          <div className=" left-[87px] top-[265px] text-right text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma] h-[80px]">
+          <div className=" left-[87px] top-[265px] text-right text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma] h-[50px]">
             <span className="pr-20 lg:pr-20 text-[#E31C25] ">7907982102</span>
           </div>
-          <div className="pl-6 left-[46px] top-[309px] text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma]">
+          <div className="h-[30px] md:h-[50px] pl-16 left-[46px] text-[#FAFAFA] text-[19px] md:text-[29px] font-bold font-[Microgramma]">
             EPHREM -
           </div>
 
@@ -72,7 +72,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className=" w-full h-[484px] rounded-[20px] overflow-hidden border-2 border-[#161616] flex flex-col justify-start items-center">
+        <div className=" w-full h-[350px] md:h-[484px] rounded-[20px] overflow-hidden border-2 border-[#161616] flex flex-col justify-start items-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.5204101451295!2d76.90376577501328!3d8.545851291497474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bec79541c519%3A0x98324eb5aafb3778!2sCollege%20of%20Engineering%20Trivandrum%20(CET)!5e0!3m2!1sen!2sin!4v1739653118757!5m2!1sen!2sin"
             // width="480"
