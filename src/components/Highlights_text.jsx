4a5cda0ca31c8e800ebf131c512ab0561e4b763a
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-02-19 11:02:29 +0530

    Butterfly effect final and highlights text

--- src/components/Highlights_text.jsx
+++ src/components/Highlights_text.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 const HighlightsText = () => {
   return (
-    <div className="flex flex-col max-w-full space-y-4 w-full relative tracking-wide  text-[#161616] text-justify sm:text-center md:text-justify  px-4 md:px-20 lg:h-[500px]">
-      
-      <p className=" text-2xl sm:text-3xl md:text-[39px] font-avantgarde font-medium !leading-[1.2]">
-      TedxCET covers a wide range of topics, from science and technology to art and design, social issues, etc.It present new ideas and brings people together to share these ideas. </p>
-<p className="text-2xl sm:text-3xl md:text-[39px] font-avantgarde font-medium !leading-[1.2]">
-TedxCET in 2018 came up with speakers like Brajmohan Das, monk and life coach, Sid Sriram, a perfect blend of  brilliance and talent, leading architect Bijoy Ramachandran, Dr Suman Sahai, Harish Sivaramakrishnan and others. They all gave open conversations about life and how to deal with it with elegance and simplicity.
-</p>
-<p className="text-2xl sm:text-3xl md:text-[39px] font-avantgarde font-medium !leading-[1.2]">
-TedxCET 2022 brought a theme of "Paradigm Shift" with the intention of bringing a radial difference of how we see the world around us. It made us think and create a new world that existed beyond our imaginations
+    <div className="space-y-6 font-avantgarde justify-center text-justify pl-7 pr-7 md:pl-12 md:pr-12 md:text-2xl  md:text-[39px] md:font-medium md:!leading-[1.2] font-medium">
+      <p>
+        TedxCET covers a wide range of topics, from science and technology to art 
+        and design, social issues, and more. It presents new ideas and brings 
+        people together to share these ideas.
+      </p>
+
+      <p>
+        TedxCET in 2018 came with speakers like Brajmohan Das, monk and life 
+        coach; Sid Sriram, a perfect blend of brilliance and talent; leading 
+        architect Bijoy Ramachandran; Dr. Suman Sahai; Harish Sivaramakrishnan 
+        and others. They all gave open conversations about life and how to deal 
+        with it with elegance and simplicity.
+      </p>
+
+      <p>
+        TedxCET 2022 brought a theme of "Paradigm Shift" with the intention of 
+        bringing a radical difference in how we see the world around us. It made 
+        us think and create a new world that existed beyond our imaginations.
       </p>
     </div>
   );
